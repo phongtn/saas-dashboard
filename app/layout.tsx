f434d7da899a3082c736338ac66b8c757b8be05b
PhongTn <phongtn.group1@gmail.com>
2024-01-24 14:06:24 +0700

    reformat to pertty

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/ui/global.css';
-import {inter, playfair} from "@/app/ui/fonts";
+import { inter, playfair } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,10 +1,10 @@
-import {Inter, Lusitana, Playfair_Display} from 'next/font/google';
-import {NextFont} from "next/dist/compiled/@next/font";
-import {undefined} from "zod";
+import { Inter, Lusitana, Playfair_Display } from 'next/font/google';
+import { NextFont } from 'next/dist/compiled/@next/font';
+import { undefined } from 'zod';
 
-export const inter = Inter({subsets: ['latin']});
-export const playfair: NextFont = Playfair_Display({subsets: ['latin']});
+export const inter = Inter({ subsets: ['latin'] });
+export const playfair: NextFont = Playfair_Display({ subsets: ['latin'] });
 export const lusitana: NextFont = Lusitana({
-    weight: ['400', '700'],
-    subsets: ['latin']
+  weight: ['400', '700'],
+  subsets: ['latin'],
 });
