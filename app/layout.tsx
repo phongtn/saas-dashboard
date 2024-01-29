c5797d0543fe91e8f116d8930573780825be8180
PhongTn <phongtn.group1@gmail.com>
2024-01-29 16:22:22 +0700

    add metadata into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css';
 import { inter, playfair } from '@/app/ui/fonts';
+import React from "react";
+import {Metadata} from "next";
+export const metadata: Metadata = {
+  title: {
+    template: '%s | SaaS Dashboard',
+    default: 'SaaS Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://saas-dashboard-roan.vercel.app/'),
+};
+
 export default function RootLayout({
   children,
 }: {
